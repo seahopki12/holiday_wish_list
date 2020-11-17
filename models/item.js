bb342c618c53e196c219bc500002e6762d6fc033
Sean Hopkins <seahopki12@gmail.com>
2020-11-16 20:55:15 -0500

    added new model for item and created relationship for both tables

---
+++ models/item.js
@@ -0,0 +1,18 @@
+module.exports = function(sequelize, DataTypes) {
+  const Item = sequelize.define("Item", {
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  });
+  Item.associate = function(models) {
+    // We're saying that a Post should belong to an Author
+    // A Post can't be created without an Author due to the foreign key constraint
+    Item.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+  return Items;
+};
